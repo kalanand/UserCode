00db5782bddaed3e58597a867d75cc8d745ce7f3
kalanand <>
2010-04-12 18:05:36 +0000

    update

--- StatTools/FitDijetMass_Data.C
+++ StatTools/FitDijetMass_Data.C
@@ -10,7 +10,7 @@
  *   Kalanand Mishra, Fermilab - kalanand@fnal.gov
  *
  * Description:
- *   Plots dijet mass distributions in data and MC and fits them to QCD lineshape 
+ *   Plots dijet mass distributions in data and fit to QCD lineshape 
  *
  * Implementation details:
  *   Uses RooFit and RooStat classes.
@@ -36,31 +36,6 @@ gSystem->Load("libRooFit") ;
 using namespace RooFit;
 
 
-//  _____ _              _   ____                                _                
-// |  ___(_)_  _____  __| | |  _ \ __ _ _ __ __ _ _ __ ___   ___| |_ ___ _ __ __ _ 
-// | |_  | \ \/ / _ \/ _` | | |_) / _` | '__/ _` | '_ ` _ \ / _ \ __/ _ \ '__/ _ _|
-// |  _| | |>  <  __/ (_| | |  __/ (_| | | | (_| | | | | | |  __/ ||  __/ |  \__ \
-// |_|   |_/_/\_\___|\__,_| |_|   \__,_|_|  \__,_|_| |_| |_|\___|\__\___|_|  |___/
-// 
-
-const int nMassBins = 29;
-double massBoundaries[nMassBins+1] = {1, 3, 6, 10, 16, 23, 31, 40, 50, 61, 
-				      74, 88, 103, 119, 137, 156, 176, 197, 
-				      220, 244, 270, 296, 325, 354, 386, 419, 
-				      453, 489, 526, 565};
-
-TCut GoodEvent = "eventVar.RunNo == 132440 || eventVar.RunNo == 132473"
-  " || eventVar.RunNo == 132599 || eventVar.RunNo == 132601 ||"
-  " eventVar.RunNo == 132602 || eventVar.RunNo == 132605";
-TCut eventCut ="eventVar.PVntracks>3 && abs(eventVar.PVz)<15"
-  " && jet1Var.corPt>20. && eventVar.njets>1 && jet2Var.corPt>20."
-  " && abs(abs(jet1Var.phi - jet2Var.phi)-3.1416)<1.";
-   
-TCut jetidCut = "jet1Var.emf>0.01 && jet2Var.emf>0.01 && jet1Var.n90hits>1"
-  " && jet2Var.n90hits>1 && jet1Var.fHPD<0.98 && jet2Var.fHPD<0.98";
-
-TCut eta_he = "abs(jet1Var.eta)<1.3 && abs(jet2Var.eta)<1.3";
-
 
 //  __  __       _         _____                 _   _             
 // |  \/  | __ _(_)_ __   |  ___|   _ _ __   ___| |_(_) ___  _ __  
@@ -72,78 +47,33 @@ TCut eta_he = "abs(jet1Var.eta)<1.3 && abs(jet2Var.eta)<1.3";
 void FitDijetMass_Data() {
 
   
-  TFile *inf  = new TFile("/uscms_data/d2/sertac/data/7TeVCollisions/Data/"
-			  "ProcessedTree_ak7calo_till_R132605.root");
-  TFile *mcinf   = new TFile("/uscms_data/d2/kkousour/7TeV/MinBias/"
-			  "ProcessedTree_ak7calo_MinBias.root");
+  TFile *inf  = new TFile("MassResults_ak7calo.root");
 
-
-  //-------------- Book Histograms ----------------------
- 
-  TH1F *hCorMass     = new TH1F("hCorMass","DijetMass",nMassBins,massBoundaries);
-  TH1F *hCorMass_mc  = new TH1F("hCorMass_mc","DijetMass",nMassBins,massBoundaries);
- 
-  TTree* tr = (TTree*) inf->Get("tr");
-  TTree* tr_mc = (TTree*) mcinf->Get("tr");
-  tr->Draw("eventVar.corMass>>hCorMass",eventCut && GoodEvent && jetidCut && eta_he,"goff");
-  tr_mc->Draw("eventVar.corMass>>hCorMass_mc",eventCut && jetidCut && eta_he,"goff");
-
-
-  // set right normalization
-  double n_mc = hCorMass_mc->GetEntries();
-  double n_data = hCorMass->GetEntries();
-   
-  hCorMass_mc->Scale(n_data/n_mc);
-  hCorMass_mc->SetFillColor(19);
-  hCorMass_mc->SetLineColor(2);
-  hCorMass_mc->SetMarkerColor(2);
-  hCorMass_mc->SetXTitle("Corrected Dijet Mass (GeV)");
-  hCorMass_mc->SetYTitle("Events / (1 GeV)");
-  hCorMass->SetMarkerStyle(20);
-  hCorMass_mc->GetXaxis()->SetRangeUser(0..,600.);
-    
-
-  for(int i=1; i<30; i++){
-    hCorMass->SetBinContent(i+1, hCorMass->GetBinContent(i+1) / hCorMass->GetBinWidth(i+1));
-    hCorMass->SetBinError(i+1, hCorMass->GetBinError(i+1) / hCorMass->GetBinWidth(i+1));
-
-    hCorMass_mc->SetBinContent(i+1, hCorMass_mc->GetBinContent(i+1) / hCorMass_mc->GetBinWidth(i+1));
-    hCorMass_mc->SetBinError(i+1, hCorMass_mc->GetBinError(i+1) / hCorMass_mc->GetBinWidth(i+1));
-
-  } 
+  TH1F *hCorMass     = (TH1F*) inf->Get("DiJetMass");
 
 
 
   // Our observable is the invariant mass
-   RooRealVar invMass("invMass", "Corrected dijet mass", 1., 565.,"GeV");
-   //  RooRealVar invMassmc("invMassmc", "Corrected dijet mass", 1., 565.,"GeV");
-   RooDataHist data( "data", "", invMass, hCorMass);
-   RooDataHist mc( "mc", "", invMass, hCorMass_mc);
+  RooRealVar invMass("invMass", "Corrected dijet mass", 
+		     45., 600.0, "GeV");
+  RooDataHist data( "data", "", invMass, hCorMass);
 
    //////////////////////////////////////////////
    // make QCD model
-   RooRealVar p1("p1","p1", -16.63, -50., 50.) ;  
-   RooRealVar p2("p2","p2", 15.3, 0., 50.) ; 
-   RooRealVar p3("p3","p3", 3.0, -10., 3.8) ; 
+  RooRealVar p1("p1","p1", 2.975, 1., 200.) ;  
+  RooRealVar p2("p2","p2", 5., 4.2, 6.) ; 
+  RooRealVar p3("p3","p3", 0., -1., 1.) ; 
+
+
+   // define QCD line shape
    RooGenericPdf qcdModel("qcdModel",
-   "pow(1-invMass/7000.,p1)/pow(invMass/7000.,p2+p3*log10(invMass/7000.))",
+   "pow(1-invMass/7000.+p3*(invMass/7000.)*(invMass/7000.),p1)/pow(invMass,p2)",
    RooArgList(invMass,p1,p2, p3)); 
-   RooRealVar p0("p0", "# events", 10.0, 0.0, 10000000.0);
+
+   RooRealVar p0("p0", "# events", 500.0, 0.0, 10000.0);
    // full model
    RooAddPdf model("model","qcd",RooArgList(qcdModel), RooArgList(p0)); 
 
-
-
-   //////////////////////////////////////////////
-   // Repeat PDF definition for MC
-   RooRealVar p1mc("p1mc","p1", -16.63, -50., 50.) ;  
-   RooRealVar p2mc("p2mc","p2", 15.3, 0., 50.) ; 
-   RooRealVar p3mc("p3mc","p3", 3.0, -10., 3.8) ; 
-   RooGenericPdf qcdModelmc("qcdModelmc",
-   "pow(1-invMass/7000.,p1mc)/pow(invMass/7000.,p2mc+p3mc*log10(invMass/7000.))",
-   RooArgList(invMass,p1mc,p2mc, p3mc)); 
-   RooRealVar p0mc("p0mc", "# events", 10.0, 0.0, 10000000.0);
-   RooAddPdf modelmc("modelmc","qcd",RooArgList(qcdModelmc), RooArgList(p0mc)); 
 
 
 
@@ -168,41 +98,19 @@ void FitDijetMass_Data() {
    // |_| |_|\__| 
 
 
-   model.fitTo(data, Hesse(kFALSE),Minos(kTRUE),
-	       PrintLevel(-1),SumW2Error(kTRUE), Extended(kTRUE), 
-	       Range(60, 500) );
+   RooFitResult* fit = model.fitTo(data, Hesse(kFALSE),Minos(kTRUE),
+	       PrintLevel(-1),SumW2Error(kTRUE), Extended(kTRUE),Save(kTRUE));
 
-   modelmc.fitTo(mc, Hesse(kFALSE),Minos(kTRUE),
-		 PrintLevel(-1),SumW2Error(kTRUE), Extended(kTRUE), 
-		 Range(60, 500) );
-
-
-
-//    TCanvas* mcNull = new TCanvas("mcNull","dummy canvas for MC",500,500);
-//    RooPlot* framemc = invMass.frame() ; 
-//    mc.plotOn(framemc ) ; 
-//    modelmc.plotOn(framemc, LineColor(kRed)) ;
-//    framemc->Draw() ;
-//    framemc->GetYaxis()->SetRangeUser(1E-3,1E+3);
-//    gPad->SetLogy();
-
+   fit->Print();
 
    //plot data 
    TCanvas* cdataNull = new TCanvas("cdataNull","fit to dijet mass",500,500);
    RooPlot* frame = invMass.frame() ; 
-   frame->addTH1(hCorMass_mc, "hist");
-   frame->addObject( &modelmc, "");
    data.plotOn(frame ) ; 
    model.plotOn(frame, LineColor(kBlue)) ;   
    model.paramOn(frame, Layout(0.4, 0.85, 0.92)); 
    TPaveText* dataPave = (TPaveText*) frame->findObject("model_paramBox");
    dataPave->SetY1(0.77);
-   modelmc.plotOn(frame, LineColor(kRed)) ;
-   modelmc.paramOn(frame, Layout(0.4, 0.65, 0.77)); 
-   mcPave->SetY1(0.62);
-   TPaveText* mcPave = (TPaveText*) frame->findObject("modelmc_paramBox");
-   mcPave->SetTextColor(2);
-   mcPave->SetLineColor(2);
    gPad->SetLogy();
    frame->GetYaxis()->SetNoExponent();
    frame->GetYaxis()->SetRangeUser(1E-3,1E+3);
--- StatTools/GenerateAndFit.C
+++ StatTools/GenerateAndFit.C
@@ -33,6 +33,13 @@
 gSystem->Load("libRooFit") ;
 using namespace RooFit;
 
+const int numEventsToGenerate = 1000000;
+
+const int nMassBins = 33;
+double massBoundaries[nMassBins+1] = { 526, 565, 606, 649, 693, 740, 788, 838, 890, 944, 1000, 1058, 
+                                       1118, 1181, 1246, 1313, 1383, 1455, 1530, 1607, 1687, 1770, 
+                                       1856, 1945, 2037, 2132, 2231, 2332, 2438, 2546, 2659, 2775, 
+                                       2895, 3019};
 
 //  __  __       _         _____                 _   _             
 // |  \/  | __ _(_)_ __   |  ___|   _ _ __   ___| |_(_) ___  _ __  
@@ -43,46 +50,41 @@ using namespace RooFit;
 
 void GenerateAndFit() {
 
+  TFile* fout = new TFile("QCDshape-generated.root","recreate");
+
   // Our observable is the invariant mass
-   RooRealVar invMass("invMass", "Corrected dijet mass", 30., 600.,"GeV");
+  RooRealVar invMass("invMass", "Corrected dijet mass", 
+  massBoundaries[0], massBoundaries[nMassBins],"GeV");
+
+
+  // set realistic mass range for generation and fitting
+  //invMass.setRange( 520.0, 3019.0 );
 
    //////////////////////////////////////////////
    // make QCD model
-   RooRealVar p1("p1","p1", 4., 0., 20.) ;  
-   RooRealVar p2("p2","p2", 5.3, 0., 50.) ; 
-   RooRealVar p3("p3","p3", -1.5, -20., 20.) ; 
+   RooRealVar p1("p1","p1", 3.975) ;  
+   RooRealVar p2("p2","p2", 5.302) ; 
+   RooRealVar p3("p3","p3", -1.51) ; 
 
    // define QCD line shape
    RooGenericPdf qcdModel("qcdModel",
    "pow(1-invMass/7000.+p3*(invMass/7000.)*(invMass/7000.),p1)/pow(invMass,p2)",
    RooArgList(invMass,p1,p2, p3)); 
-   // number of events
-   RooRealVar p0("p0", "# events", 1.0, 0.0, 100000000000000.0);
-   // full model
-   RooAddPdf model("model","qcd",RooArgList(qcdModel), RooArgList(p0)); 
-
-   //set the precision for the integral of the model here
-   RooNumIntConfig* cfg = RooAbsReal::defaultIntegratorConfig();
-   cfg->setEpsAbs(1E-4);
-   cfg->setEpsRel(1E-4);
-   model.setIntegratorConfig(*cfg);
-
-
-  // fix the paramters for now ==> we want to generate events
-  // p0.setVal(1.);
-   p1.setVal(3.975);
-   p2.setVal(5.302);
-   p3.setVal(-1.51);
-
-   p0.setConstant(kTRUE);
-   p1.setConstant(kTRUE);
-   p2.setConstant(kTRUE);
-   p3.setConstant(kTRUE);
-
 
   // generate data
+   RooDataSet* data = qcdModel.generate(RooArgList(invMass), numEventsToGenerate);
 
-   RooDataSet* data = model.generate(RooArgList(invMass), 1000000);
+   //////////////////////////////////////////////////////////////////////////
+   //////////////////////////////////////////////////////////////////////////
+   // the following is needed only if you want to perform a binned LH fit
+   // create binned data 
+   TTree* tree = data->tree();
+   TH1D* hist =  new TH1D("hist", "", nMassBins, massBoundaries);
+   tree->Draw("invMass>>hist", "", "goff");
+   RooDataHist binneddata( "binneddata", "", invMass, hist);
+   //////////////////////////////////////////////////////////////////////////
+   //////////////////////////////////////////////////////////////////////////
+
 
    //   __ _ _    
    //  / _(_) |_  
@@ -90,39 +92,73 @@ void GenerateAndFit() {
    // |  _| | |_  
    // |_| |_|\__| 
 
-   p0.setConstant(kFALSE);
-   p1.setConstant(kFALSE);
-   p2.setConstant(kFALSE);
-   p3.setConstant(kFALSE);
+   // number of events
+   RooRealVar p0("p0", "# events", 10000.0, 0., 1.0e+6);
+   // full model
+   RooAddPdf model("model","qcd",RooArgList(qcdModel), RooArgList(p0)); 
 
-   model.fitTo(*data, Hesse(kFALSE),PrintLevel(-1), Range(40, 220));
+   //set the precision for the integral of the model here
+   RooNumIntConfig* cfg = RooAbsReal::defaultIntegratorConfig();
+   cfg->setEpsAbs(1E-5);
+   cfg->setEpsRel(1E-5);
+   model.setIntegratorConfig(*cfg);
+
+   p1.setConstant(kFALSE);
+   p1.setRange(4., 6.) ; 
+   
+   p2.setConstant(kFALSE);
+   p2.setRange(4.5, 5.5) ; 
+   p3.setConstant(kFALSE);
+   p3.setRange(-20., -1.) ; 
+
+   ////// unbinned likelihood fit
+     // RooFitResult* fit = model.fitTo( *data, PrintLevel(-1),Save(kTRUE));
+
+   ////// binned likelihood fit
+   RooFitResult* fit = model.fitTo(binneddata,SumW2Error(kTRUE), PrintLevel(-1),Save(kTRUE));
+
+   fit->Print();
+   Double_t NLLatMLE = fit->minNll();
 
    //plot data and fit 
    gROOT->ProcessLine(".L tdrstyle.C");
    setTDRStyle();
-   tdrStyle->SetErrorX(0.5);
-   tdrStyle->SetPadLeftMargin(0.18);
    tdrStyle->SetPadRightMargin(0.08);
-   tdrStyle->SetLegendBorderSize(0);
-   tdrStyle->SetTitleYOffset(1.3);
-   gStyle->SetOptFit(1111);
-   tdrStyle->SetOptFile(1111);
-   tdrStyle->SetOptStat(0); 
-   tdrStyle->SetStatColor(1);
+
 
    TCanvas* canvas = new TCanvas("canvas","fit to dijet mass",500,500);
-   invMass.setRange(20,300);
-   RooPlot* frame = invMass.frame(0,400, 50) ; 
-   data->plotOn(frame ) ; 
+   RooPlot* frame = invMass.frame() ; 
+   /////// unbinned likelihood fit
+   //data.plotOn(frame ) ; 
+   /////// unbinned likelihood fit
+   binneddata.plotOn(frame ) ; 
    model.plotOn(frame, LineColor(kBlue)) ;   
-   char title[100];
-   cout << frame->chiSquare() << endl;
-   sprintf(title, "#chi^{2}/#nu  = %.2f", frame->chiSquare());
-   TLatex* tex = new TLatex(47.17749,0.4662757,title);
-   tex->Draw();
-   gPad->SetLogy();
-   frame->GetYaxis()->SetNoExponent();
    frame->Draw() ;
+   frame->SetMinimum(0.2);
+   frame->SetMaximum(1e+07);
+   frame->GetYaxis()->SetTitle("Events/bin");
+   gPad->SetLogy();
+   char title[100];
+   cout << "chi^2 of the fit is: " << frame->chiSquare() << endl;
+   sprintf(title, "NLL = %.1E,  #chi^{2}/#nu  = %.2f/%d", NLLatMLE, frame->chiSquare(), nMassBins);
+   TLatex tex;
+   tex.SetNDC();
+   tex.SetTextAlign(12);
+   tex.SetTextSize(0.04);
+   tex.DrawLatex(0.35,0.87, title);
+   canvas->SaveAs("myCanvas.root");
+   canvas->SaveAs("myCanvas.eps");
+   canvas->SaveAs("myCanvas.gif");
+
+   fout->cd();
+   hist->Write();
+
+   ////// delete all created objects to avoid memory leak //////
+   // delete hist;
+   // fout->Close();
+   // delete fout;
+   // delete canvas;
+   // delete data;
 }
 
 
